b05c697f0f70121ea2852a2c7809e1bc69b6bea3
zahadneokurkycz <filip.belzik@email.cz>
2024-05-17 19:19:57 +0200

    Edited my lib a little bit

--- libs/themer.js
+++ libs/themer.js
@@ -1,30 +1,51 @@
-const SetTheme = (theme) => {
+var Themer = {};
+Themer.SetTheme = (theme) => {
 	localStorage.setItem('theme', theme)
 	if (theme == 'auto') {
-		SetAutoTheme()
+		Themer.SetAutoTheme()
 		return;
 	}
 	document.getElementsByTagName('html')[0].setAttribute('data-bs-theme', theme)
 }
 
-const SetAutoTheme = () => {
+Themer.SetAutoTheme = () => {
 	const date = new Date();
 	if (date.getHours() > 8 && date.getHours() < 19) {
-		SetTheme('light')
+		Themer.SetTheme('light')
 	} else {
-		SetTheme('dark')
+		Themer.SetTheme('dark')
 	}
 }
 
-const CheckThemes = () => {
+Themer.CheckThemes = () => {
+	Themer.CreateElement()
 	if (localStorage.getItem('theme') == 'auto') {
-		SetAutoTheme()
+		Themer.SetAutoTheme()
 	} else if (localStorage.getItem('theme') == 'light' || localStorage.getItem('theme') == 'dark') {
-		SetTheme(localStorage.getItem('theme'))
+		Themer.SetTheme(localStorage.getItem('theme'))
 	} else {
-		SetAutoTheme()
+		Themer.SetAutoTheme()
 		localStorage.setItem('theme', 'auto')
 	}
 }
 
-document.addEventListener("load", CheckThemes());
+Themer.CreateElement = () => {
+	try {
+		const themerEl = document.getElementsByTagName('themer-btn')[0]
+		themerEl.innerHTML = `<div class="dropdown-center">
+		<button class="` + themerEl.getAttribute('btnclass') + ` dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+		  Theme
+		</button>
+		<ul class="dropdown-menu">
+		  <li><a class="dropdown-item" href="#" onclick="Themer.SetTheme('light')"><i class="bi bi-brightness-high"></i> Light</a></li>
+		  <li><a class="dropdown-item" href="#" onclick="Themer.SetTheme('dark')"><i class="bi bi-moon-stars"></i> Dark</a></li>
+		  <li><a class="dropdown-item" href="#" onclick="Themer.SetTheme('auto')"><i class="bi bi-circle-half"></i> Auto</a></li>
+		</ul>
+	  </div>`
+	} catch(e) {
+		//Do nothing
+	}
+}
+
+
+document.addEventListener("load", 'Themer.CheckThemes(); Themer.CreateElement()');
